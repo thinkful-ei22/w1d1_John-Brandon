b7e50e530631865551abb7506d3b7cbc101de1b0
Olyskoop <snap@olymega.org>
2018-06-12 15:51:55 -0700

    streamlined in mentor session

--- minMax.js
+++ minMax.js
@@ -2,35 +2,21 @@
 
 let numArray = [1,2,3,4,5,6];
 
-
-
 let max = function(numbers){
-	let i = 0;
 	let max = numbers[0];
-	while (i < numbers.length){
-		if(numbers[i] > max){
-			max = numbers[i];
-		}
-		i++;
+	for(num of numbers){
+		num > max ? max=num : 0
 	}
-	//return largest number in the array
 	return max;
 }
 
 console.log(max(numArray));
 
 
-
-
 const min = function(numbers){
-	let i = 0;
 	let min = numbers[0];
-
-	while (i < numbers.length) {
-		if (numbers[i] < min){
-			min = numbers[i];
-		}
-		i++;
+	for(num of numbers){
+		num < max ? min=num : 0
 	}
 	return min;
 	//return smallest number in the array
