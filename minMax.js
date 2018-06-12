2ec285ee5f9b30b056be93a8a349d1576a6e9583
Brandon Graham <bgraham452@gmail.com>
2018-06-12 12:21:06 -0400

    added min func

--- minMax.js
+++ minMax.js
@@ -23,6 +23,17 @@ console.log(max(numArray));
 
 
 const min = function(numbers){
+	let i = 0;
+	let min = numbers[0];
+
+	while (i < numbers.length) {
+		if (numbers[i] < min){
+			min = numbers[i];
+		}
+		i++;
+	}
+	return min;
 	//return smallest number in the array
 }
 
+console.log(min(numArray));
