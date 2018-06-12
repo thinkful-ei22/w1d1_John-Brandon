4eed2f39a6bae150ea6eedb688d82865ed324356
Brandon Graham <bgraham452@gmail.com>
2018-06-12 12:31:20 -0400

    added repeat function

--- drills.js
+++ drills.js
@@ -1,28 +1,18 @@
-//Redo the "max and min (without sort or Math.max/min)" drill but this time use a while loop instead of a for loop
-
-let numArray = [1,2,3,4,5,6];
-
-
-
-let max = function(numbers){
-	let i = 0;
-	let max = numbers[0];
-	while (i < numbers.length){
-		if(numbers[i] > max){
-			max = numbers[i];
-		}
-		i++;
+function repeat (fn, n){
+	for (let i =0; i < n; i++){
+		fn();
 	}
-	//return largest number in the array
-	return max;
 }
 
-console.log(max(numArray));
-
-
-
-
-const min = function(numbers){
-	//return smallest number in the array
+function hello (){
+	console.log('Hello World');
 }
+
+function goodbye (){
+	console.log('Goodbye world');
+}
+
+repeat(hello, 5);
+
+repeat(goodbye, 5);
 
