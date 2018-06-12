c60de29a9261337b67a266e25a39fb44a73ad957
Olyskoop <snap@olymega.org>
2018-06-12 15:37:18 -0700

    olyskoop final submit

--- reduction.js
+++ reduction.js
@@ -1,12 +1,8 @@
 
 function reduction(input){
-	let words = input.split(' ');
-	const reducer = (accumulator, currentValue) => accumulator + currentValue;
-	//Push the upper-cased last letter of each word to the product array if it's not 3 in length
-	const product = words.map(word => 
-		word.length === 3 ? ' ' : word[word.length-1].toUpperCase()
-	);		
-	return(product.reduce(reducer));
+	return input.split(' ').reduce((accumulator, word) => 
+		accumulator + (word.length === 3 ? ' ' : word[word.length-1].toUpperCase()), ''
+	);	
 }
 
 let input = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
